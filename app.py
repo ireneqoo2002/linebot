117cee820ab749ad887dcc38a2a0281f6e87cb34
ireneqoo2002 <138761687+ireneqoo2002@users.noreply.github.com>
2023-08-07 11:52:55 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -26,8 +26,32 @@ def callback():
 
 @handler.add(MessageEvent,message=TextMessage)
 def handle_message(event):
-    message=TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    #message=TextSendMessage(text=event.message.text)
+    #line_bot_api.reply_message(event.reply_token,message)
+    
+    emoji = [
+        {
+            "index":0,
+            "productId": "5ac1bfd5040ab15980c9b435"
+            "emojiId":"009"
+        },
+        {
+            "index":17,
+            "productId": "5ac2211e031a6752fb806d61"
+            "emojiId":"003"
+        }
+    ]
 
+    text_message=TextSendMessage(text='''$Master Finance $
+Hello! 您好，這是一個分析股票、油價的頻道！''',emojis=emoji)
+
+    sticker_message = StickerMessage(
+        package_id='8522'
+        sticker_id='16581271'
+    )
+    line_bot_api.reply_message(
+        event.reply_token,
+        [text_message,sticker_message])
+    
 if __name__=="__main__":
     app.run()
