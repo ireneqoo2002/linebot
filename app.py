e61abfc13168b795836a3a397b14cfbd62a94d66
ireneqoo2002 <138761687+ireneqoo2002@users.noreply.github.com>
2023-08-09 13:53:38 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -61,9 +61,6 @@ def handle_messahe(event):
         stockNumber = msg[2:6]
         content = write_my_stock(uid, user_name,stockNumber,msg[6:7],msg[7:])
         line_bot_api.push_message(uid, TextSendMessage(content)) 
-     else:
-        content = write_my_stock(uid,user_name,stockNumber,"未設定","未設定")
-        line_bot_api.push_message(uid,TextSendMessage(content))
         return 0
      if (emsg.startswith('#')):
         text = emsg[1:]
