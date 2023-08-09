b8de5c968e789878ec45a5dbae3d43faa41b7096
ireneqoo2002 <138761687+ireneqoo2002@users.noreply.github.com>
2023-08-09 10:13:36 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from line_bot_api import *
 from events.basic import *
 from events.oil import *
+from events.Msg_Template import *
+import re
+import twstock
+import datetime
 
 app=Flask(__name__)
 
@@ -20,28 +24,68 @@ def callback():
         abort(400)
 
     return 'OK'
-
+#處理訊息
 @handler.add(MessageEvent,message=TextMessage)
 def handle_messahe(event):
-     profile=line_bot_api.get_profile(event.source.user_id)
-     uid=profile.user_id
+     profile = line_bot_api.get_profile(event.source.user_id)
+     uid = profile.user_id
      message_text = str(event.message.text).lower()
+     msg = str(event.message.text).upper().strip()
+     emsg = event.message.text
 
-     #################################################
+     #使用說明############################################
 
      if message_text =='@使用說明':
          about_us_event(event)
 
-     if event.message.text =="想知道油價":
+    #油價################################################
+     if event.message.text =="油價":
          content = oil_price()
          line_bot_api.reply_message(
              event.reply_token,
              TextSendMessage(text=content)
          )
-    ##################################################
+    #股價################################################
      if event.message.text=="股價查詢":
         line_bot_api.push_message(uid,TextSendMessage("請輸入#加股票代號......"))
+    #股價查詢
+     if re.match("想知道股價[0-9]",msg):
+         stockNumber = msg[2:6]
+         btn_msg =stock_reply_other(stockNumber)
+         line_bot_api.push_message(uid,btn_msg)
+         return 0
+     if(emsg.startwish('#')):
+         text = emsg[1:]
+         content=''
 
+         stock_rt = twstock.realtime.get(text)
+         my_datetime = datetime.datetime.fromtimestamp(stock_rt['timestamp']+8*60*60)
+         my_time = my_datetime.strftime('%H:%M:%S')
+
+         content += '%s (%s) %s\n' %(
+             stock_rt['info']['name'],
+             stock_rt['info']['code'],
+            my_time)        
+         content += '現價: %s / 開盤: %s\n'%(
+             stock_rt['realtime']['latest_trabe_price'],
+             stock_rt['realtime']['open'])
+         content += '最高: %s / 最低: %s\n'%( 
+             stock_rt['realtime']['high'],
+             stock_rt['realtime']['low'])
+         content += '量: %s\n' %(stock_rt['realtime']['accumulate_trade_volume']) 
+
+         stock=twstock.Stock(text)
+         content += '-----\n'
+         content += '最近五日價格: \n'
+         price5 = stock.price[-5:][::-1]
+         date5 = stock.date[-5:][::-1]
+         for i in range(len(price5)):
+             content +='[%s] %s\n' %(date5[i].strftime("%Y-%m+%d"),price5[i])
+         line_bot_api.reply_message(
+             event.reply_token,
+             TextSendMessage(text=content)
+         )
+    #股價################################################
      if message_text =='@小幫手':
          Usage(event)
      if event.message.text =="@小幫手":
@@ -70,7 +114,7 @@ def handle_messahe(event):
         line_bot_api.reply_message(
             event.reply_token,
             [buttons_template])
-#################################################
+#封鎖################################################
 
 @handler.add(FollowEvent)
 def handle_unfollow(event):
